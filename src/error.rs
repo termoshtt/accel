4fcea3a8e906d2c7bb1f7794d0aed1fc23363cee
Toshiki Teramura <toshiki.teramura@gmail.com>
2017-11-13 01:13:22 +0900

    Add error submod

---
+++ src/error.rs
@@ -0,0 +1,115 @@
+#![allow(non_camel_case_types)]
+
+pub use ffi::cuda::cudaError_enum as cudaError;
+
+pub type Result<T> = ::std::result::Result<T, Error>;
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash, IntoEnum)]
+pub enum Error {
+    cudaError(cudaError),
+    cudaRuntimeError(cudaRuntimeError),
+}
+
+pub fn check<E: Into<Error>>(e: E) -> Result<()> {
+    match e.into() {
+        Error::cudaError(e) => {
+            if e != cudaError::CUDA_SUCCESS {
+                return Err(e.into());
+            }
+        }
+        Error::cudaRuntimeError(e) => {
+            if e != cudaRuntimeError::Success {
+                return Err(e.into());
+            }
+        }
+    }
+    Ok(())
+}
+
+#[repr(u32)]
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
+pub enum cudaRuntimeError {
+    Success = 0,
+    MissingConfiguration = 1,
+    MemoryAllocation = 2,
+    InitializationError = 3,
+    LaunchFailure = 4,
+    PriorLaunchFailure = 5,
+    LaunchTimeout = 6,
+    LaunchOutOfResources = 7,
+    InvalidDeviceFunction = 8,
+    InvalidConfiguration = 9,
+    InvalidDevice = 10,
+    InvalidValue = 11,
+    InvalidPitchValue = 12,
+    InvalidSymbol = 13,
+    MapBufferObjectFailed = 14,
+    UnmapBufferObjectFailed = 15,
+    InvalidHostPointer = 16,
+    InvalidDevicePointer = 17,
+    InvalidTexture = 18,
+    InvalidTextureBinding = 19,
+    InvalidChannelDescriptor = 20,
+    InvalidMemcpyDirection = 21,
+    AddressOfConstant = 22,
+    TextureFetchFailed = 23,
+    TextureNotBound = 24,
+    SynchronizationError = 25,
+    InvalidFilterSetting = 26,
+    InvalidNormSetting = 27,
+    MixedDeviceExecution = 28,
+    CudartUnloading = 29,
+    Unknown = 30,
+    NotYetImplemented = 31,
+    MemoryValueTooLarge = 32,
+    InvalidResourceHandle = 33,
+    NotReady = 34,
+    InsufficientDriver = 35,
+    SetOnActiveProcess = 36,
+    InvalidSurface = 37,
+    NoDevice = 38,
+    ECCUncorrectable = 39,
+    SharedObjectSymbolNotFound = 40,
+    SharedObjectInitFailed = 41,
+    UnsupportedLimit = 42,
+    DuplicateVariableName = 43,
+    DuplicateTextureName = 44,
+    DuplicateSurfaceName = 45,
+    DevicesUnavailable = 46,
+    InvalidKernelImage = 47,
+    NoKernelImageForDevice = 48,
+    IncompatibleDriverContext = 49,
+    PeerAccessAlreadyEnabled = 50,
+    PeerAccessNotEnabled = 51,
+    DeviceAlreadyInUse = 54,
+    ProfilerDisabled = 55,
+    ProfilerNotInitialized = 56,
+    ProfilerAlreadyStarted = 57,
+    ProfilerAlreadyStopped = 58,
+    Assert = 59,
+    TooManyPeers = 60,
+    HostMemoryAlreadyRegistered = 61,
+    HostMemoryNotRegistered = 62,
+    OperatingSystem = 63,
+    PeerAccessUnsupported = 64,
+    LaunchMaxDepthExceeded = 65,
+    LaunchFileScopedTex = 66,
+    LaunchFileScopedSurf = 67,
+    SyncDepthExceeded = 68,
+    LaunchPendingCountExceeded = 69,
+    NotPermitted = 70,
+    NotSupported = 71,
+    HardwareStackError = 72,
+    IllegalInstruction = 73,
+    MisalignedAddress = 74,
+    InvalidAddressSpace = 75,
+    InvalidPc = 76,
+    IllegalAddress = 77,
+    InvalidPtx = 78,
+    InvalidGraphicsContext = 79,
+    NvlinkUncorrectable = 80,
+    JitCompilerNotFound = 81,
+    CooperativeLaunchTooLarge = 82,
+    StartupFailure = 127,
+    ApiFailureBase = 10000,
+}
--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,11 @@
 //! ==============================
 //!
 
+#[macro_use]
+extern crate procedurals;
 extern crate glob;
 extern crate cuda_sys as ffi;
 
 pub mod ptx_builder;
 pub mod uvec;
+pub mod error;

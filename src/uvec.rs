c5deb7ef0a696c72ee7e1858fecaabef62d38dc6
Toshiki Teramura <toshiki.teramura@gmail.com>
2017-11-13 23:11:05 +0900

    Add memset

--- src/uvec.rs
+++ src/uvec.rs
@@ -1,11 +1,12 @@
 
-use ffi::cuda_runtime as rt;
+use ffi::cuda_runtime::*;
 use super::error::*;
 
 use std::os::raw::*;
 use std::slice::{from_raw_parts, from_raw_parts_mut};
 use std::ops::{Deref, DerefMut, Index, IndexMut};
 use std::ptr::null_mut;
+use std::mem::size_of;
 
 #[derive(Debug)]
 pub struct UVec<T> {
@@ -14,14 +15,27 @@ pub struct UVec<T> {
 }
 
 impl<T> UVec<T> {
-    pub fn new(n: usize) -> Result<Self> {
+    pub unsafe fn uninitialized(n: usize) -> Result<Self> {
         let mut ptr: *mut c_void = null_mut();
-        unsafe { rt::cudaMallocManaged(&mut ptr as *mut *mut c_void, n, rt::cudaMemAttachGlobal) }
-            .check()?;
+        cudaMallocManaged(
+            &mut ptr as *mut *mut c_void,
+            n * size_of::<T>(),
+            cudaMemAttachGlobal,
+        ).check()?;
         Ok(UVec {
             ptr: ptr as *mut T,
             n,
         })
+    }
+
+    pub fn fill_zero(&mut self) -> Result<()> {
+        unsafe { cudaMemset(self.ptr as *mut c_void, 0, self.n * size_of::<T>()).check() }
+    }
+
+    pub fn new(n: usize) -> Result<Self> {
+        let mut v = unsafe { Self::uninitialized(n) }?;
+        v.fill_zero()?;
+        Ok(v)
     }
 
     /// Recast to Rust's immutable slice
@@ -63,7 +77,7 @@ impl<T> IndexMut<usize> for UVec<T> {
 
 impl<T> Drop for UVec<T> {
     fn drop(&mut self) {
-        unsafe { rt::cudaFree(self.ptr as *mut c_void) }
+        unsafe { cudaFree(self.ptr as *mut c_void) }
             .check()
             .expect("Free failed");
     }
@@ -72,13 +86,14 @@ impl<T> Drop for UVec<T> {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use sync::*;
 
     #[test]
     fn uvec_new() {
-        let n = 1024;
-        let mut uv: UVec<f64> = UVec::new(n).unwrap();
-        for i in 0..n {
-            uv[i] = 0.0;
+        // zero-filled on GPU
+        let v: UVec<f64> = UVec::new(128).unwrap();
+        for val in v.iter() {
+            println!("{}", val);
         }
     }
 }

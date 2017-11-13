e32c4ab804884017d436b03e1c7c9cfcf6ed1b45
Toshiki Teramura <toshiki.teramura@gmail.com>
2017-11-13 16:54:24 +0900

    Revert "Drop CUDA_LIBRARY_PATH"
    This reverts commit a3aa0db0982a94d5f800b9be2fd7152cf8a32e7f.

--- cuda-sys/build.rs
+++ cuda-sys/build.rs
@@ -1,6 +1,12 @@
 
+use std::env;
+
 fn main() {
-    println!("cargo:rerun-if-changed=build.rs");
+    match env::var("CUDA_LIBRARY_PATH") {
+        Ok(path) => println!("cargo:rustc-link-search=native={}", path),
+        Err(_) => {}
+    };
     println!("cargo:rustc-link-lib=dylib=cuda");
     println!("cargo:rustc-link-lib=dylib=cudart");
+    println!("cargo:rerun-if-changed=build.rs");
 }

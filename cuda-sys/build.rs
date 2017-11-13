a3aa0db0982a94d5f800b9be2fd7152cf8a32e7f
Toshiki Teramura <toshiki.teramura@gmail.com>
2017-11-13 16:09:04 +0900

    Drop CUDA_LIBRARY_PATH

--- cuda-sys/build.rs
+++ cuda-sys/build.rs
@@ -1,12 +1,6 @@
 
-use std::env;
-
 fn main() {
-    match env::var("CUDA_LIBRARY_PATH") {
-        Ok(path) => println!("cargo:rustc-link-search=native={}", path),
-        Err(_) => {}
-    };
+    println!("cargo:rerun-if-changed=build.rs");
     println!("cargo:rustc-link-lib=dylib=cuda");
     println!("cargo:rustc-link-lib=dylib=cudart");
-    println!("cargo:rerun-if-changed=build.rs");
 }

f68c9bdf77076da008f7b9ab8564c7d090834b36
Toshiki Teramura <toshiki.teramura@gmail.com>
2017-11-29 20:21:57 +0900

    Add wrapper for cuLinkAdd{Data,File}

--- src/kernel.rs
+++ src/kernel.rs
@@ -4,9 +4,113 @@ use ffi::vector_types::*;
 use error::*;
 
 use std::ptr::null_mut;
-use std::os::raw::{c_char, c_void};
+use std::os::raw::{c_uint, c_char, c_void};
 use std::str::FromStr;
-use std::path::Path;
+use std::path::{Path, PathBuf};
+use std::collections::HashMap;
+
+pub type JITOption = Option<HashMap<CUjit_option, *mut c_void>>;
+
+fn parse(option: &JITOption) -> (c_uint, Vec<CUjit_option>, Vec<*mut c_void>) {
+    if let &Some(ref hmap) = option {
+        let opt_name: Vec<_> = hmap.keys().cloned().collect();
+        let opt_value = hmap.values().cloned().collect();
+        (opt_name.len() as c_uint, opt_name, opt_value)
+    } else {
+        (0, Vec::new(), Vec::new())
+    }
+}
+
+pub enum Data {
+    PTX(String),
+    PTXFile(PathBuf),
+    CubinFile(PathBuf),
+}
+
+impl Data {
+    fn input_type(&self) -> CUjitInputType {
+        match *self {
+            Data::PTX(_) | Data::PTXFile(_) => CUjitInputType_enum::CU_JIT_INPUT_PTX,
+            Data::CubinFile(_) => CUjitInputType_enum::CU_JIT_INPUT_CUBIN,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct Linker(CUlinkState);
+
+impl Drop for Linker {
+    fn drop(&mut self) {
+        unsafe { cuLinkDestroy(self.0) }.check().expect(
+            "Failed to release Linker",
+        );
+    }
+}
+
+impl Linker {
+    pub fn create(option: &JITOption) -> Result<Self> {
+        let (n, mut opt, mut opts) = parse(option);
+        let mut st = null_mut();
+        unsafe { cuLinkCreate_v2(n, opt.as_mut_ptr(), opts.as_mut_ptr(), &mut st as *mut _) }
+            .check()?;
+        Ok(Linker(st))
+    }
+
+    unsafe fn add_data(
+        &mut self,
+        input_type: CUjitInputType,
+        data: *mut c_void,
+        size: usize,
+        opt: &JITOption,
+    ) -> Result<()> {
+        let (nopts, mut opts, mut opt_vals) = parse(opt);
+        let name = str2cstring(&"\0");
+        cuLinkAddData_v2(
+            self.0,
+            input_type,
+            data,
+            size,
+            name.as_ptr(),
+            nopts,
+            opts.as_mut_ptr(),
+            opt_vals.as_mut_ptr(),
+        ).check()?;
+        Ok(())
+    }
+
+    unsafe fn add_file(
+        &mut self,
+        input_type: CUjitInputType,
+        path: &Path,
+        opt: &JITOption,
+    ) -> Result<()> {
+        let filename = str2cstring(path.to_str().unwrap()).as_mut_ptr();
+        let (nopts, mut opts, mut opt_vals) = parse(opt);
+        cuLinkAddFile_v2(
+            self.0,
+            input_type,
+            filename,
+            nopts,
+            opts.as_mut_ptr(),
+            opt_vals.as_mut_ptr(),
+        ).check()?;
+        Ok(())
+    }
+
+    pub fn add(&mut self, data: &Data, opt: &JITOption) -> Result<()> {
+        match *data {
+            Data::PTX(ref ptx) => unsafe {
+                let (ptr, n) = str2void(ptx);
+                self.add_data(data.input_type(), ptr, n, opt)?;
+            },
+            Data::PTXFile(ref path) |
+            Data::CubinFile(ref path) => unsafe {
+                self.add_file(data.input_type(), path, opt)?;
+            },
+        };
+        Ok(())
+    }
+}
 
 #[derive(Debug)]
 pub struct PTXModule(CUmodule);
@@ -118,4 +222,9 @@ impl Grid {
 fn str2cstring(s: &str) -> Vec<c_char> {
     let cstr = String::from_str(s).unwrap() + "\0";
     cstr.into_bytes().into_iter().map(|c| c as c_char).collect()
+}
+
+unsafe fn str2void(s: &str) -> (*mut c_void, usize) {
+    let mut cstr = str2cstring(s);
+    (cstr.as_mut_ptr() as *mut _, cstr.len())
 }

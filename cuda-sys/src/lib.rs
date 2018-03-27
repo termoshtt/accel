b46e4daf5822fcac35ceb53f11d8e1710a02cd05
Toshiki Teramura <toshiki.teramura@gmail.com>
2018-03-28 00:21:08 +0900

    Split test in cudart

--- cuda-sys/src/lib.rs
+++ cuda-sys/src/lib.rs
@@ -1,6 +1,6 @@
 pub mod vector_types;
 pub mod cuda;
-pub mod cuda_runtime;
+pub mod cudart;
 pub mod cublas;
 
 #[test]
@@ -13,3 +13,4 @@ fn cuda_version() {
 }
 
 mod cuda_tests;
+mod cudart_tests;

ccbc627925486a40c2c88802698743d287f29555
Toshiki Teramura <toshiki.teramura@gmail.com>
2018-04-03 21:27:10 +0900

    Test nvptx code (failes)

--- accel-derive/src/build.rs
+++ accel-derive/src/build.rs
@@ -146,11 +146,16 @@ impl CheckRun for process::Command {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use config::*;
 
     #[test]
     fn compile() {
         let src = r#"
-        pub unsafe fn add(a: *const f64, b: *const f64, c: *mut f64, n: usize) {
+        #![feature(abi_ptx)]
+        #![no_std]
+        extern crate accel_core;
+        #[no_mangle]
+        pub unsafe extern "ptx-kernel" fn add(a: *const f64, b: *const f64, c: *mut f64, n: usize) {
             let i = accel_core::index();
             if (i as usize) < n {
                 *c.offset(i) = *a.offset(i) + *b.offset(i);

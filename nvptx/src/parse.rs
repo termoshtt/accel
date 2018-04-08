3faa4a9c2bf68d4f4c0a8eda2297a77c96bf0a81
Toshiki Teramura <toshiki.teramura@gmail.com>
2018-04-09 05:36:46 +0900

    Rename crate attribute

--- nvptx/src/parse.rs
+++ nvptx/src/parse.rs
@@ -1,8 +1,6 @@
 use proc_macro::TokenStream;
 use syn::*;
 use std::path::*;
-use std::env;
-
 use config::Crate;
 use compile::Builder;
 
@@ -14,14 +12,13 @@ pub fn parse_func(func: TokenStream) -> ItemFn {
 ///
 /// For attributes are allowed:
 ///
-/// - `depends`: add dependent crate
-///    - `#[depends("accel-core")]` equals to `accel-core = "*"` in Cargo.toml
-///    - `#[depends("accel-core" = "0.1.0")]` equals to `accel-core = "0.1.0"`
-/// - `depends_path`: add dependent crate from local
-///    - `#[depends_path("accel-core" = "/some/path")]`
+/// - `crate`: add dependent crate
+///    - `#[crate("accel-core")]` equals to `accel-core = "*"` in Cargo.toml
+///    - `#[crate("accel-core" = "0.1.0")]` equals to `accel-core = "0.1.0"`
+/// - `crate_path`: add dependent crate from local
+///    - `#[crate_path("accel-core" = "/some/path")]`
 ///      equals to `accel-core = { path = "/some/path" }`
 /// - `#[build_path("/some/path")]`: build PTX on "/some/path"
-/// - `#[build_path_home("path/to/work")]`: build PTX on "$HOME/path/to/work"
 ///
 pub fn parse_builder_attrs(attrs: &[Attribute]) -> Builder {
     let mut crates = Vec::new();
@@ -33,10 +30,9 @@ pub fn parse_builder_attrs(attrs: &[Attribute]) -> Builder {
         let tts = quote!{#tts}.to_string();
         let attr = tts.trim_matches(PENE);
         match path.as_str() {
-            "crates" => crates.push(depends_to_crate(&attr)),
-            "depends_path" => crates.push(depends_path_to_crate(&attr)),
+            "crate" => crates.push(as_crate(&attr)),
+            "crate_path" => crates.push(as_crate_path(&attr)),
             "build_path" => build_path = Some(as_build_path(&attr)),
-            "build_path_home" => build_path = Some(as_build_path_home(&attr)),
             _ => unreachable!("Unsupported attribute: {:?}", path),
         }
     }
@@ -53,23 +49,18 @@ fn as_build_path(path: &str) -> PathBuf {
     PathBuf::from(path.trim_matches(QUOTE))
 }
 
-fn as_build_path_home(path: &str) -> PathBuf {
-    let path = path.trim_matches(QUOTE);
-    env::home_dir().expect("No home dir").join(path).to_owned()
-}
-
-fn depends_to_crate(dep: &str) -> Crate {
+fn as_crate(dep: &str) -> Crate {
     let tokens: Vec<_> = dep.split('=').map(|s| s.trim_matches(QUOTE)).collect();
     match tokens.len() {
-        // #[depends("accel-core")] case
+        // #[crate("accel-core")] case
         1 => Crate::new(tokens[0]),
-        // #[depends("accel-core" = "0.1.0")] case
+        // #[crate("accel-core" = "0.1.0")] case
         2 => Crate::with_version(tokens[0], tokens[1]),
         _ => unreachable!("Invalid line: {}", dep),
     }
 }
 
-fn depends_path_to_crate(dep: &str) -> Crate {
+fn as_crate_path(dep: &str) -> Crate {
     let tokens: Vec<_> = dep.split('=').map(|s| s.trim_matches(QUOTE)).collect();
     match tokens.len() {
         // #[depends_path("accel-core" = "/some/path")] case

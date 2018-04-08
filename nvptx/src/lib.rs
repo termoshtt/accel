4fa8a1c326e0188ef72eee20b3220588b301f992
Toshiki Teramura <toshiki.teramura@gmail.com>
2018-04-09 03:32:32 +0900

    Split nvptx crate from accel-derive

---
+++ nvptx/src/lib.rs
@@ -0,0 +1,21 @@
+#![recursion_limit = "128"]
+
+extern crate glob;
+extern crate proc_macro;
+#[macro_use]
+extern crate procedurals;
+#[macro_use]
+extern crate derive_more;
+#[macro_use]
+extern crate quote;
+extern crate serde;
+#[macro_use]
+extern crate serde_derive;
+extern crate syn;
+extern crate tempdir;
+extern crate toml;
+
+pub mod config;
+pub mod build;
+pub mod parse;
+pub mod generate;

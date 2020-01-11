19ccad4b452705233139ade89688ace81e543b16
Toshiki Teramura <toshiki.teramura@gmail.com>
2020-01-11 16:47:35 +0900

    Use accel_core

--- accel-derive/tests/kernels/dependencies.rs
+++ accel-derive/tests/kernels/dependencies.rs
@@ -2,10 +2,14 @@ use accel_derive::kernel;
 
 #[kernel]
 #[dependencies("accel-core" = "0.3.0-alpha.1")]
-unsafe fn version() {}
+unsafe fn version() {
+    let _i = accel_core::index();
+}
 
 #[kernel]
 #[dependencies("accel-core" = { version = "0.3.0-alpha.1" })]
-unsafe fn version_table() {}
+unsafe fn version_table() {
+    let _i = accel_core::index();
+}
 
 fn main() {}

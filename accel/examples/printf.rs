9c154b5888422420837322bc724583ec5d3b11e3
Toshiki Teramura <toshiki.teramura@gmail.com>
2020-01-13 01:11:21 +0000

    Add printf example

---
+++ accel/examples/printf.rs
@@ -0,0 +1,13 @@
+use accel::*;
+use accel_derive::kernel;
+
+#[kernel]
+pub unsafe fn print() {
+    core::arch::nvptx::vprintf("Hello GPU World!".as_ptr(), core::ptr::null_mut());
+}
+
+fn main() {
+    let grid = Grid::x(1);
+    let block = Block::x(4);
+    print(grid, block);
+}

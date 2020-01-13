644a91d4cbc931976c68a27268952d6b423ce95c
Toshiki Teramura <toshiki.teramura@gmail.com>
2020-01-13 18:21:27 +0000

    Add dummy vector on unified memory for implicit initialization

--- accel/examples/printf.rs
+++ accel/examples/printf.rs
@@ -7,6 +7,8 @@ pub unsafe fn print() {
 }
 
 fn main() {
+    let _a: UVec<f64> = UVec::new(4).unwrap();
+
     let grid = Grid::x(1);
     let block = Block::x(4);
     print(grid, block).expect("print kernel failed");

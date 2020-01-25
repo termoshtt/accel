82eb0c961bb8d0c771acb3c1340e4647e861c531
Toshiki Teramura <toshiki.teramura@gmail.com>
2020-01-25 08:31:18 +0000

    Add PTXAllocator
    Make public

--- accel-core/src/lib.rs
+++ accel-core/src/lib.rs
@@ -1,7 +1,21 @@
 #![feature(stdsimd)]
 #![no_std]
 
+extern crate alloc;
+
+use alloc::alloc::*;
 use core::arch::nvptx;
+
+pub struct PTXAllocator;
+
+unsafe impl GlobalAlloc for PTXAllocator {
+    unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
+        nvptx::malloc(layout.size()) as *mut u8
+    }
+    unsafe fn dealloc(&self, ptr: *mut u8, _layout: Layout) {
+        nvptx::free(ptr as *mut _);
+    }
+}
 
 pub struct Dim3 {
     pub x: i32,

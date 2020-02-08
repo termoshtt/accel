42517c210170499351530826e1e3720f1e06c6a7
Toshiki Teramura <toshiki.teramura@gmail.com>
2020-02-09 04:40:40 +0900

    Update examples

--- accel/examples/printf.rs
+++ accel/examples/printf.rs
@@ -11,7 +11,8 @@ fn main() -> anyhow::Result<()> {
     let grid = Grid::x(1);
     let block = Block::x(4);
     let device = driver::Device::nth(0)?;
-    let _ctx = device.create_context_auto()?;
+    let ctx = device.create_context_auto()?;
+    let _guard = ctx.set()?;
     print(grid, block)?;
     Ok(())
 }

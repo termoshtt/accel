5cca80dcd043c983122c9e8d31756bb8f5051eef
Toshiki Teramura <toshiki.teramura@gmail.com>
2020-02-22 15:37:33 +0900

    Add sub.cu

--- accel/tests/data/add.cu
+++ accel/tests/data/add.cu
@@ -1,8 +1,4 @@
-__device__ int f(int a) {
-  return 2 * a;
-}
-
 __global__ void add(const int a[], const int b[], int c[]) {
     int i = blockDim.x * blockIdx.x + threadIdx.x;
-    c[i] = f(a[i] + b[i]);
+    c[i] = a[i] + b[i];
 }

6c2e3595cb1995663016dbc77f596751b217fa73
Toshiki Teramura <toshiki.teramura@gmail.com>
2020-02-29 15:10:41 +0900

    Expose PTX string

--- accel-derive/src/host.rs
+++ accel-derive/src/host.rs
@@ -1,4 +1,6 @@
+use inflections::Inflect;
 use proc_macro::TokenStream;
+use proc_macro2::Span;
 use quote::quote;
 
 pub fn func2caller(ptx_str: &str, func: &syn::ItemFn) -> TokenStream {
@@ -18,8 +20,13 @@ pub fn func2caller(ptx_str: &str, func: &syn::ItemFn) -> TokenStream {
         })
         .collect();
     let kernel_name = quote! { #ident }.to_string();
+    let ptx_const_name = syn::Ident::new(
+        &format!("{}_ptx", kernel_name).to_constant_case(),
+        Span::call_site(),
+    );
 
     let caller = quote! {
+        #vis const #ptx_const_name: &'static str = #ptx_str;
         #vis #fn_token #ident(grid: accel::Grid, block: accel::Block, #inputs) -> anyhow::Result<()> {
             use accel::driver::{
                 kernel::void_cast,
@@ -28,7 +35,7 @@ pub fn func2caller(ptx_str: &str, func: &syn::ItemFn) -> TokenStream {
             };
             let device = Device::nth(0)?;
             let ctx = device.create_context_auto()?;
-            let module = Module::from_str(&ctx, #ptx_str)?;
+            let module = Module::from_str(&ctx, #ptx_const_name)?;
             let mut kernel = module.get_kernel(#kernel_name)?;
             let mut args = [#(void_cast(&#input_values)),*];
             unsafe { kernel.launch(args.as_mut_ptr(), grid, block)? };

262597f342b73ae65a14c3bc3f4726247cb07667
Toshiki Teramura <toshiki.teramura@gmail.com>
2020-03-10 20:09:56 +0900

    impl as_slice and as_mut_slice

--- accel/src/driver/memory.rs
+++ accel/src/driver/memory.rs
@@ -2,7 +2,7 @@ use super::context::*;
 use crate::{ffi_call_unsafe, ffi_new_unsafe};
 use anyhow::{ensure, Result};
 use cuda::*;
-use std::mem::MaybeUninit;
+use std::{marker::PhantomData, mem::MaybeUninit};
 
 pub use cuda::CUmemAttach_flags_enum as AttachFlag;
 
@@ -36,44 +36,66 @@ impl MemoryInfo {
 }
 
 /// Memory allocated on the device.
-pub struct DeviceMemory {
+pub struct DeviceMemory<'ctx, T> {
     ptr: CUdeviceptr,
     size: usize,
+    ctx: &'ctx Context,
+    phantom: PhantomData<T>,
 }
 
-impl Drop for DeviceMemory {
+impl<'ctx, T> Drop for DeviceMemory<'ctx, T> {
     fn drop(&mut self) {
         ffi_call_unsafe!(cuMemFree_v2, self.ptr).expect("Failed to free device memory");
     }
 }
 
-impl DeviceMemory {
-    /// Allocate a new device memory with `size` byte length by [cuMemAlloc].
+impl<'ctx, T> DeviceMemory<'ctx, T> {
+    /// Allocate a new device memory by [cuMemAlloc].
     /// This memory is not managed by the unified memory system.
     ///
     /// [cuMemAlloc]: https://docs.nvidia.com/cuda/cuda-driver-api/group__CUDA__MEM.html#group__CUDA__MEM_1gb82d2a09844a58dd9e744dc31e8aa467
-    pub fn non_managed(ctx: &Context, size: usize) -> Result<Self> {
+    pub fn non_managed(ctx: &'ctx Context, size: usize) -> Result<Self> {
         ensure!(ctx.is_current()?, "Given context must be current");
-        let ptr = ffi_new_unsafe!(cuMemAlloc_v2, size)?;
-        Ok(DeviceMemory { ptr, size })
+        let ptr = ffi_new_unsafe!(cuMemAlloc_v2, size * std::mem::size_of::<T>())?;
+        Ok(DeviceMemory {
+            ptr,
+            size,
+            ctx,
+            phantom: PhantomData,
+        })
     }
 
     /// Allocate a new device memory with `size` byte length by [cuMemAllocManaged].
     /// This memory is managed by the unified memory system.
     ///
     /// [cuMemAllocManaged]: https://docs.nvidia.com/cuda/cuda-driver-api/group__CUDA__MEM.html#group__CUDA__MEM_1gb82d2a09844a58dd9e744dc31e8aa467
-    pub fn managed(ctx: &Context, size: usize, flag: AttachFlag) -> Result<Self> {
+    pub fn managed(ctx: &'ctx Context, size: usize, flag: AttachFlag) -> Result<Self> {
         ensure!(ctx.is_current()?, "Given context must be current");
-        let ptr = ffi_new_unsafe!(cuMemAllocManaged, size, flag as u32)?;
-        Ok(DeviceMemory { ptr, size })
+        let ptr = ffi_new_unsafe!(
+            cuMemAllocManaged,
+            size * std::mem::size_of::<T>(),
+            flag as u32
+        )?;
+        Ok(DeviceMemory {
+            ptr,
+            size,
+            ctx,
+            phantom: PhantomData,
+        })
     }
 
-    /// Length of Device memory (in bytes)
+    /// Length of device memory
     pub fn len(&self) -> usize {
         self.size
     }
 
+    /// Size of device memory in bytes
+    pub fn byte_size(&self) -> usize {
+        self.size * std::mem::size_of::<T>()
+    }
+
     fn get_attr<Attr>(&self, attr: CUpointer_attribute) -> Result<Attr> {
+        ensure!(self.ctx.is_current()?, "Given context must be current");
         let ty = MaybeUninit::uninit();
         ffi_call_unsafe!(cuPointerGetAttribute, ty.as_ptr() as *mut _, attr, self.ptr)?;
         let ty = unsafe { ty.assume_init() };
@@ -99,6 +121,24 @@ impl DeviceMemory {
     pub fn is_mapped(&self) -> Result<bool> {
         self.get_attr(CUpointer_attribute::CU_POINTER_ATTRIBUTE_MAPPED)
     }
+
+    /// Access as a slice. This returns error if not managed
+    pub fn as_slice(&self) -> Result<&[T]> {
+        ensure!(
+            self.is_managed()?,
+            "Device memory cannot be accessed from host if not managed"
+        );
+        Ok(unsafe { std::slice::from_raw_parts(self.ptr as *const T, self.size) })
+    }
+
+    /// Access as a mutable slice. This returns error if not managed
+    pub fn as_mut_slice(&self) -> Result<&mut [T]> {
+        ensure!(
+            self.is_managed()?,
+            "Device memory cannot be accessed from host if not managed"
+        );
+        Ok(unsafe { std::slice::from_raw_parts_mut(self.ptr as *mut T, self.size) })
+    }
 }
 
 #[cfg(test)]
@@ -118,11 +158,25 @@ mod tests {
     }
 
     #[test]
-    fn new() -> Result<()> {
+    fn managed() -> Result<()> {
         let device = Device::nth(0)?;
         let ctx = device.create_context_auto()?;
-        let mem = DeviceMemory::non_managed(&ctx, 12)?;
+        let mem = DeviceMemory::<i32>::managed(&ctx, 12, AttachFlag::CU_MEM_ATTACH_GLOBAL)?;
         assert_eq!(mem.len(), 12);
+        assert_eq!(mem.byte_size(), 12 * 4 /* size of i32 */);
+        let sl = mem.as_mut_slice()?;
+        sl[0] = 3;
+        Ok(())
+    }
+
+    #[test]
+    fn non_managed() -> Result<()> {
+        let device = Device::nth(0)?;
+        let ctx = device.create_context_auto()?;
+        let mem = DeviceMemory::<i32>::non_managed(&ctx, 12)?;
+        assert_eq!(mem.len(), 12);
+        assert_eq!(mem.byte_size(), 12 * 4 /* size of i32 */);
+        assert!(mem.as_slice().is_err());
         Ok(())
     }
 
@@ -132,14 +186,14 @@ mod tests {
         let ctx = device.create_context_auto()?;
 
         // non-managed
-        let mem1 = DeviceMemory::non_managed(&ctx, 12)?;
+        let mem1 = DeviceMemory::<i32>::non_managed(&ctx, 12)?;
         dbg!(mem1.buffer_id()?);
         assert_eq!(mem1.memory_type()?, MemoryType::CU_MEMORYTYPE_DEVICE);
         assert!(!mem1.is_managed()?);
         assert!(mem1.is_mapped()?);
 
         // managed
-        let mem2 = DeviceMemory::managed(&ctx, 12, AttachFlag::CU_MEM_ATTACH_GLOBAL)?;
+        let mem2 = DeviceMemory::<i32>::managed(&ctx, 12, AttachFlag::CU_MEM_ATTACH_GLOBAL)?;
         assert_eq!(mem2.memory_type()?, MemoryType::CU_MEMORYTYPE_DEVICE);
         assert!(mem2.is_managed()?);
         assert!(mem2.is_mapped()?);

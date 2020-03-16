b619eb0dcb87db59784f28edc4464eaa4fb846db
Toshiki Teramura <toshiki.teramura@gmail.com>
2020-03-17 03:35:42 +0900

    Update slice example

--- accel/examples/slice.rs
+++ accel/examples/slice.rs
@@ -1,19 +1,23 @@
-use accel::*;
+use accel::driver::{device::*, memory::*, *};
 use accel_derive::kernel;
+use anyhow::Result;
 
 #[kernel]
-pub fn add(a: &[f64], b: &[f64], c: &mut [f64]) {
+pub fn add_slice(a: &[f64], b: &[f64], c: &mut [f64]) {
     let i = accel_core::index() as usize;
     if (i as usize) < c.len() {
         c[i] = a[i] + b[i];
     }
 }
 
-fn main() -> anyhow::Result<()> {
+fn main() -> Result<()> {
+    let device = Device::nth(0)?;
+    let ctx = device.create_context_auto()?;
+
     let n = 32;
-    let mut a = UVec::new(n).unwrap();
-    let mut b = UVec::new(n).unwrap();
-    let mut c = UVec::new(n).unwrap();
+    let mut a = DeviceMemory::<f64>::managed(&ctx, n, AttachFlag::CU_MEM_ATTACH_GLOBAL)?;
+    let mut b = DeviceMemory::<f64>::managed(&ctx, n, AttachFlag::CU_MEM_ATTACH_GLOBAL)?;
+    let mut c = DeviceMemory::<f64>::managed(&ctx, n, AttachFlag::CU_MEM_ATTACH_GLOBAL)?;
 
     for i in 0..n {
         a[i] = i as f64;
@@ -24,9 +28,13 @@ fn main() -> anyhow::Result<()> {
 
     let grid = Grid::x(1);
     let block = Block::x(n as u32);
-    add(grid, block, a.as_slice(), b.as_slice(), c.as_slice_mut())?;
+    add_slice(
+        &ctx,
+        grid,
+        block,
+        &(a.as_slice(), b.as_slice(), c.as_slice_mut()),
+    )?;
 
-    device::sync()?;
     println!("c = {:?}", c.as_slice());
     Ok(())
 }

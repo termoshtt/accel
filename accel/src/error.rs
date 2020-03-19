fd2d2cdfbe446d92cedd844acddde5eed6f92cbb
Toshiki Teramura <toshiki.teramura@gmail.com>
2020-03-20 05:44:25 +0900

    Move ffi_* macros into error module

--- accel/src/error.rs
+++ accel/src/error.rs
@@ -1,14 +1,18 @@
 pub use cuda::cudaError_enum as DeviceError;
 pub use cudart::cudaError_t as RuntimeError;
 
+pub type Result<T> = ::std::result::Result<T, AccelError>;
+
 #[derive(thiserror::Error, Debug, Clone, PartialEq, Eq, Hash)]
 pub enum AccelError {
+    /// Raw errors originates from CUDA Device APIs
     #[error("CUDA Device API Error: {api_name}, {error:?}")]
     Device {
         api_name: String,
         error: DeviceError,
     },
 
+    /// Raw errors originates from CUDA Device APIs
     #[error("CUDA Runtime API Error: {api_name}, {error:?}")]
     Runtime {
         api_name: String,
@@ -18,11 +22,11 @@ pub enum AccelError {
 
 /// Convert return code of CUDA Driver/Runtime API into Result
 pub(crate) trait Check {
-    fn check(self, api_name: &str) -> Result<(), AccelError>;
+    fn check(self, api_name: &str) -> Result<()>;
 }
 
 impl Check for DeviceError {
-    fn check(self, api_name: &str) -> Result<(), AccelError> {
+    fn check(self, api_name: &str) -> Result<()> {
         if self == DeviceError::CUDA_SUCCESS {
             Ok(())
         } else {
@@ -35,7 +39,7 @@ impl Check for DeviceError {
 }
 
 impl Check for RuntimeError {
-    fn check(self, api_name: &str) -> Result<(), AccelError> {
+    fn check(self, api_name: &str) -> Result<()> {
         if self == RuntimeError::cudaSuccess {
             Ok(())
         } else {
@@ -45,4 +49,50 @@ impl Check for RuntimeError {
             })
         }
     }
+}
+
+#[macro_export]
+macro_rules! ffi_call {
+    ($ffi:path, $($args:expr),*) => {
+        $ffi($($args),*).check(stringify!($ffi))
+    };
+    ($ffi:path) => {
+        $ffi().check(stringify!($ffi))
+    };
+}
+
+#[macro_export]
+macro_rules! ffi_call_unsafe {
+    ($ffi:path, $($args:expr),*) => {
+        unsafe { $crate::error::Check::check($ffi($($args),*), stringify!($ffi)) }
+    };
+    ($ffi:path) => {
+        unsafe { $crate::error::Check::check($ffi(), stringify!($ffi)) }
+    };
+}
+
+#[macro_export]
+macro_rules! ffi_new {
+    ($ffi:path, $($args:expr),*) => {
+        {
+            let mut value = ::std::mem::MaybeUninit::uninit();
+            $crate::error::Check::check($ffi(value.as_mut_ptr(), $($args),*), stringify!($ffi)).map(|_| value.assume_init())
+        }
+    };
+    ($ffi:path) => {
+        {
+            let mut value = ::std::mem::MaybeUninit::uninit();
+            $crate::error::Check::check($ffi(value.as_mut_ptr()), stringify!($ffi)).map(|_| value.assume_init())
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! ffi_new_unsafe {
+    ($ffi:path, $($args:expr),*) => {
+        unsafe { $crate::ffi_new!($ffi, $($args),*) }
+    };
+    ($ffi:path) => {
+        unsafe { $crate::ffi_new!($ffi) }
+    };
 }

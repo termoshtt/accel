30058cdf2b0a20778cb10f8008ef27918da451dc
Toshiki Teramura <toshiki.teramura@gmail.com>
2020-03-23 14:09:07 +0900

    Move get_attr to CudaMemory

--- accel/src/driver/memory.rs
+++ accel/src/driver/memory.rs
@@ -1,5 +1,5 @@
 use super::context::*;
-use crate::{error::*, ffi_call_unsafe, ffi_new_unsafe};
+use crate::{error::*, ffi_call, ffi_call_unsafe, ffi_new_unsafe};
 use cuda::*;
 use std::{
     marker::PhantomData,
@@ -7,7 +7,7 @@ use std::{
     ops::{Deref, DerefMut},
 };
 
-pub use cuda::CUmemAttach_flags_enum as AttachFlag;
+use cuda::CUmemAttach_flags_enum as AttachFlag;
 
 /// Each variants correspond to the following:
 ///
@@ -46,52 +46,96 @@ impl MemoryInfo {
 pub trait CudaMemory<T>: Deref<Target = [T]> + DerefMut {
     /// Length of device memory
     fn len(&self) -> usize;
+
     /// Size of device memory in bytes
     fn byte_size(&self) -> usize {
         self.len() * std::mem::size_of::<T>()
     }
+
     /// Get the unified address of the memory as an immutable pointer
     fn as_ptr(&self) -> *const T;
+
     /// Get the unified address of the memory as a mutable pointer
     fn as_mut_ptr(&mut self) -> *mut T;
+
     /// Access as a slice.
     fn as_slice(&self) -> &[T] {
         unsafe { std::slice::from_raw_parts(self.as_ptr(), self.len()) }
     }
+
     /// Access as a mutable slice.
     fn as_mut_slice(&mut self) -> &mut [T] {
         unsafe { std::slice::from_raw_parts_mut(self.as_mut_ptr(), self.len()) }
     }
+
+    /// Unique identifier of the memory
+    ///
+    /// ```
+    /// # use ::accel::driver::{device::*, context::*, memory::*};
+    /// # let device = Device::nth(0).unwrap();
+    /// # let ctx = device.create_context_auto().unwrap();
+    /// let mem1 = DeviceMemory::<i32>::new(&ctx, 12);
+    /// let mem2 = DeviceMemory::<i32>::new(&ctx, 12);
+    /// assert_ne!(mem1.id(), mem2.id());
+    /// ```
+    fn id(&self) -> u64 {
+        get_attr(
+            self.as_ptr(),
+            CUpointer_attribute::CU_POINTER_ATTRIBUTE_BUFFER_ID,
+        )
+    }
+
+    /// Memory Type
+    fn memory_type(&self) -> MemoryType {
+        get_attr(
+            self.as_ptr(),
+            CUpointer_attribute::CU_POINTER_ATTRIBUTE_MEMORY_TYPE,
+        )
+    }
+}
+
+// Typed wrapper of cuPointerGetAttribute
+fn get_attr<T, Attr>(ptr: *const T, attr: CUpointer_attribute) -> Attr {
+    let data = MaybeUninit::uninit();
+    unsafe {
+        ffi_call!(
+            cuPointerGetAttribute,
+            data.as_ptr() as *mut _,
+            attr,
+            ptr as CUdeviceptr
+        )
+        .expect("Cannot get pointer attributes");
+        data.assume_init()
+    }
 }
 
 /// Memory allocated on the device.
-pub struct DeviceMemory<'ctx, T> {
+pub struct DeviceMemory<T> {
     ptr: CUdeviceptr,
     size: usize,
-    ctx: &'ctx Context,
     phantom: PhantomData<T>,
 }
 
-impl<'ctx, T> Drop for DeviceMemory<'ctx, T> {
+impl<T> Drop for DeviceMemory<T> {
     fn drop(&mut self) {
         ffi_call_unsafe!(cuMemFree_v2, self.ptr).expect("Failed to free device memory");
     }
 }
 
-impl<'ctx, T> Deref for DeviceMemory<'ctx, T> {
+impl<T> Deref for DeviceMemory<T> {
     type Target = [T];
     fn deref(&self) -> &[T] {
         self.as_slice()
     }
 }
 
-impl<'ctx, T> DerefMut for DeviceMemory<'ctx, T> {
+impl<T> DerefMut for DeviceMemory<T> {
     fn deref_mut(&mut self) -> &mut [T] {
         self.as_mut_slice()
     }
 }
 
-impl<'ctx, T> CudaMemory<T> for DeviceMemory<'ctx, T> {
+impl<T> CudaMemory<T> for DeviceMemory<T> {
     fn len(&self) -> usize {
         self.size
     }
@@ -105,101 +149,25 @@ impl<'ctx, T> CudaMemory<T> for DeviceMemory<'ctx, T> {
     }
 }
 
-impl<'ctx, T> DeviceMemory<'ctx, T> {
+impl<T> DeviceMemory<T> {
     /// Allocate a new device memory with `size` byte length by [cuMemAllocManaged].
     /// This memory is managed by the unified memory system.
     ///
     /// [cuMemAllocManaged]: https://docs.nvidia.com/cuda/cuda-driver-api/group__CUDA__MEM.html#group__CUDA__MEM_1gb82d2a09844a58dd9e744dc31e8aa467
-    pub fn new(ctx: &'ctx Context, size: usize, flag: AttachFlag) -> Result<Self> {
-        ctx.assure_current()?;
+    pub fn new(ctx: &Context, size: usize) -> Self {
+        ctx.assure_current()
+            .expect("DeviceMemory::new requires valid and current context");
         let ptr = ffi_new_unsafe!(
             cuMemAllocManaged,
             size * std::mem::size_of::<T>(),
-            flag as u32
-        )?;
-        Ok(DeviceMemory {
+            AttachFlag::CU_MEM_ATTACH_GLOBAL as u32
+        )
+        .expect("Cannot allocate device memory");
+        DeviceMemory {
             ptr,
             size,
-            ctx,
             phantom: PhantomData,
-        })
-    }
-
-    fn get_attr<Attr>(&self, attr: CUpointer_attribute) -> Result<Attr> {
-        self.ctx.assure_current()?;
-        let ty = MaybeUninit::uninit();
-        ffi_call_unsafe!(cuPointerGetAttribute, ty.as_ptr() as *mut _, attr, self.ptr)?;
-        let ty = unsafe { ty.assume_init() };
-        Ok(ty)
-    }
-
-    /// Unique ID of the memory
-    pub fn buffer_id(&self) -> Result<u64> {
-        self.get_attr(CUpointer_attribute::CU_POINTER_ATTRIBUTE_BUFFER_ID)
-    }
-
-    /// Check if the memory is managed by the unified memory system
-    pub fn assure_managed(&self) -> Result<()> {
-        if self.get_attr::<bool>(CUpointer_attribute::CU_POINTER_ATTRIBUTE_IS_MANAGED)? {
-            Ok(())
-        } else {
-            Err(AccelError::DeviceMemoryIsNotManaged)
         }
-    }
-
-    pub fn memory_type(&self) -> Result<MemoryType> {
-        self.get_attr(CUpointer_attribute::CU_POINTER_ATTRIBUTE_MEMORY_TYPE)
-    }
-
-    /// Check if this pointer is in a valid address range that is mapped to a backing allocation.
-    /// This will always returns true
-    pub fn is_mapped(&self) -> Result<bool> {
-        self.get_attr(CUpointer_attribute::CU_POINTER_ATTRIBUTE_MAPPED)
-    }
-}
-
-#[cfg(test)]
-mod device_memory_tests {
-    use super::super::device::*;
-    use super::*;
-
-    #[test]
-    fn info() -> Result<()> {
-        let device = Device::nth(0)?;
-        let ctx = device.create_context_auto()?;
-        let mem_info = MemoryInfo::get(&ctx)?;
-        dbg!(&mem_info);
-        assert!(mem_info.free > 0);
-        assert!(mem_info.total > mem_info.free);
-        Ok(())
-    }
-
-    #[test]
-    fn managed() -> Result<()> {
-        let device = Device::nth(0)?;
-        let ctx = device.create_context_auto()?;
-        let mut mem = DeviceMemory::<i32>::new(&ctx, 12, AttachFlag::CU_MEM_ATTACH_GLOBAL)?;
-        assert_eq!(mem.len(), 12);
-        assert_eq!(mem.byte_size(), 12 * 4 /* size of i32 */);
-        let sl = mem.as_mut_slice();
-        sl[0] = 3;
-        Ok(())
-    }
-
-    #[test]
-    fn pointer_attributes() -> Result<()> {
-        let device = Device::nth(0)?;
-        let ctx = device.create_context_auto()?;
-
-        let mem1 = DeviceMemory::<i32>::new(&ctx, 12, AttachFlag::CU_MEM_ATTACH_GLOBAL)?;
-        assert_eq!(mem1.memory_type()?, MemoryType::CU_MEMORYTYPE_DEVICE);
-        assert!(mem1.assure_managed().is_ok());
-        assert!(mem1.is_mapped()?);
-
-        // Buffer id of two different memory must be different
-        let mem2 = DeviceMemory::<i32>::new(&ctx, 12, AttachFlag::CU_MEM_ATTACH_GLOBAL)?;
-        assert_ne!(mem1.buffer_id()?, mem2.buffer_id()?);
-        Ok(())
     }
 }
 
@@ -254,12 +222,58 @@ impl<T> PageLockedMemory<T> {
 }
 
 #[cfg(test)]
-mod page_locaked_memory_tests {
+mod tests {
+    use super::super::device::*;
     use super::*;
 
-    #[should_panic]
     #[test]
-    fn zero_new() {
-        let _a: PageLockedMemory<i32> = PageLockedMemory::new(0);
+    fn info() -> Result<()> {
+        let device = Device::nth(0)?;
+        let ctx = device.create_context_auto()?;
+        let mem_info = MemoryInfo::get(&ctx)?;
+        dbg!(&mem_info);
+        assert!(mem_info.free > 0);
+        assert!(mem_info.total > mem_info.free);
+        Ok(())
+    }
+
+    #[should_panic(expected = "Cannot allocate")]
+    #[test]
+    fn page_locked_new_zero() {
+        let _a = PageLockedMemory::<i32>::new(0);
+    }
+
+    #[should_panic(expected = "Cannot allocate")]
+    #[test]
+    fn device_new_zero() {
+        let device = Device::nth(0).unwrap();
+        let ctx = device.create_context_auto().unwrap();
+        let _a = DeviceMemory::<i32>::new(&ctx, 0);
+    }
+
+    #[test]
+    fn device() -> Result<()> {
+        let device = Device::nth(0)?;
+        let ctx = device.create_context_auto()?;
+        let mut mem = DeviceMemory::<i32>::new(&ctx, 12);
+        assert_eq!(mem.len(), 12);
+        assert_eq!(mem.byte_size(), 12 * 4 /* size of i32 */);
+        let sl = mem.as_mut_slice();
+        sl[0] = 3;
+        Ok(())
+    }
+
+    #[test]
+    fn pointer_attributes() -> Result<()> {
+        let device = Device::nth(0)?;
+        let ctx = device.create_context_auto()?;
+
+        let mem1 = DeviceMemory::<i32>::new(&ctx, 12);
+        assert_eq!(mem1.memory_type(), MemoryType::CU_MEMORYTYPE_DEVICE);
+
+        // Buffer id of two different memory must be different
+        let mem2 = DeviceMemory::<i32>::new(&ctx, 12);
+        assert_ne!(mem1.id(), mem2.id());
+        Ok(())
     }
 }

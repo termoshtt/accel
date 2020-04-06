ff112637e4d65de1478d00a5559bd802f52cea48
Toshiki Teramura <toshiki.teramura@gmail.com>
2020-04-07 05:11:41 +0900

    Split MemoryInfo

--- accel/src/memory/device.rs
+++ accel/src/memory/device.rs
@@ -18,46 +18,6 @@ use cuda::CUmemAttach_flags_enum as AttachFlag;
 /// - Unified device or host memory
 pub use cuda::CUmemorytype_enum as MemoryType;
 
-/// Total and Free memory size of the device (in bytes)
-#[derive(Debug, Clone, Copy, PartialEq)]
-struct MemoryInfo {
-    free: usize,
-    total: usize,
-}
-
-impl MemoryInfo {
-    fn get(ctx: &Context) -> Self {
-        let _gurad = ctx.guard_context();
-        let mut free = 0;
-        let mut total = 0;
-        ffi_call!(
-            cuMemGetInfo_v2,
-            &mut free as *mut usize,
-            &mut total as *mut usize
-        )
-        .expect("Cannot get memory info");
-        MemoryInfo { free, total }
-    }
-}
-
-/// Get total memory size in bytes of the current device
-///
-/// Panic
-/// ------
-/// - when given context is not current
-pub fn total_memory(ctx: &Context) -> usize {
-    MemoryInfo::get(ctx).total
-}
-
-/// Get free memory size in bytes of the current device
-///
-/// Panic
-/// ------
-/// - when given context is not current
-pub fn free_memory(ctx: &Context) -> usize {
-    MemoryInfo::get(ctx).free
-}
-
 /// Trait for CUDA managed memories. It assures
 ///
 /// - can be accessed from both host (CPU) and device (GPU) programs
@@ -295,17 +255,6 @@ impl<'ctx, T> PageLockedMemory<'ctx, T> {
 mod tests {
     use super::*;
     use crate::error::*;
-
-    #[test]
-    fn info() -> Result<()> {
-        let device = Device::nth(0)?;
-        let ctx = device.create_context();
-        let mem_info = MemoryInfo::get(&ctx);
-        dbg!(&mem_info);
-        assert!(mem_info.free > 0);
-        assert!(mem_info.total > mem_info.free);
-        Ok(())
-    }
 
     #[test]
     fn page_locked() -> Result<()> {
--- accel/src/memory/mod.rs
+++ accel/src/memory/mod.rs
@@ -22,3 +22,63 @@ pub mod device;
 
 pub use array::*;
 pub use device::*;
+
+use crate::{device::*, ffi_call};
+use cuda::*;
+
+/// Total and Free memory size of the device (in bytes)
+#[derive(Debug, Clone, Copy, PartialEq)]
+struct MemoryInfo {
+    free: usize,
+    total: usize,
+}
+
+impl MemoryInfo {
+    fn get(ctx: &Context) -> Self {
+        let _gurad = ctx.guard_context();
+        let mut free = 0;
+        let mut total = 0;
+        ffi_call!(
+            cuMemGetInfo_v2,
+            &mut free as *mut usize,
+            &mut total as *mut usize
+        )
+        .expect("Cannot get memory info");
+        MemoryInfo { free, total }
+    }
+}
+
+/// Get total memory size in bytes of the current device
+///
+/// Panic
+/// ------
+/// - when given context is not current
+pub fn total_memory(ctx: &Context) -> usize {
+    MemoryInfo::get(ctx).total
+}
+
+/// Get free memory size in bytes of the current device
+///
+/// Panic
+/// ------
+/// - when given context is not current
+pub fn free_memory(ctx: &Context) -> usize {
+    MemoryInfo::get(ctx).free
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::error::*;
+
+    #[test]
+    fn info() -> Result<()> {
+        let device = Device::nth(0)?;
+        let ctx = device.create_context();
+        let mem_info = MemoryInfo::get(&ctx);
+        dbg!(&mem_info);
+        assert!(mem_info.free > 0);
+        assert!(mem_info.total > mem_info.free);
+        Ok(())
+    }
+}

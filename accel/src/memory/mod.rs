ae0fc87b79390c56c1addb37a046ecc22d37aaa0
Toshiki Teramura <toshiki.teramura@gmail.com>
2020-04-07 05:09:43 +0900

    Summarize into a table

--- accel/src/memory/mod.rs
+++ accel/src/memory/mod.rs
@@ -1,17 +1,20 @@
 //! Memory management
 //!
-//! There are several types of memories to be considered:
+//! Unified address
+//! ---------------
 //!
-//! - (usual) Host memory
-//!   - allocated by usual manner, e.g. `vec![0; n]`
-//! - registered Host memory
-//!   - allocated by usual manner, and registered into CUDA unified memory system
-//! - Page-locked Host memory
-//!   - OS memory paging feature is disabled for accelarating memory transfer
-//! - Device memory
-//!   - allocated on device as a single span
-//! - Array
-//!   - properly aligned memory on device for using Texture and Surface memory
+//! - All memories are mapped into a single 64bit memory space
+//!
+//! Memory Types
+//! ------------
+//!
+//! |name                     | where exists | From Host | From Device | Description                                                               |
+//! |:------------------------|:------------:|:---------:|:-----------:|:--------------------------------------------------------------------------|
+//! | (usual) Host memory     | Host         | ✓         |  -          | allocated by usual manner, e.g. `vec![0; n]`                              |
+//! | registered Host memory  | Host         | ✓         |  ✓          | allocated by usual manner, and registered into CUDA unified memory system |
+//! | Page-locked Host memory | Host         | ✓         |  ✓          | OS memory paging feature is disabled for accelarating memory transfer     |
+//! | Device memory           | Device       | ✓         |  ✓          | allocated on device as a single span                                      |
+//! | Array                   | Device       | ✓         |  ✓          | properly aligned memory on device for using Texture and Surface memory    |
 //!
 
 pub mod array;

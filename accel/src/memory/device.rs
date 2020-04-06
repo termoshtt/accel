b6783b81371c8e04d2376f6b06a75c4e469daafd
Toshiki Teramura <toshiki.teramura@gmail.com>
2020-04-07 05:14:28 +0900

    Move CudaMemory

--- accel/src/memory/device.rs
+++ accel/src/memory/device.rs
@@ -1,10 +1,10 @@
 //! Device and Host memory handlers
 
+use super::CudaMemory;
 use crate::{device::*, ffi_call, ffi_new};
 use cuda::*;
 use std::{
     marker::PhantomData,
-    mem::MaybeUninit,
     ops::{Deref, DerefMut},
 };
 
@@ -17,85 +17,6 @@ use cuda::CUmemAttach_flags_enum as AttachFlag;
 /// - Array memory
 /// - Unified device or host memory
 pub use cuda::CUmemorytype_enum as MemoryType;
-
-/// Trait for CUDA managed memories. It assures
-///
-/// - can be accessed from both host (CPU) and device (GPU) programs
-/// - can be treated as a slice, i.e. a single span of either host or device memory
-///
-pub trait CudaMemory<T>: Deref<Target = [T]> + DerefMut {
-    /// Length of device memory
-    fn len(&self) -> usize;
-
-    /// Size of device memory in bytes
-    fn byte_size(&self) -> usize {
-        self.len() * std::mem::size_of::<T>()
-    }
-
-    /// Get the unified address of the memory as an immutable pointer
-    fn as_ptr(&self) -> *const T;
-
-    /// Get the unified address of the memory as a mutable pointer
-    fn as_mut_ptr(&mut self) -> *mut T;
-
-    /// Access as a slice.
-    fn as_slice(&self) -> &[T] {
-        unsafe { std::slice::from_raw_parts(self.as_ptr(), self.len()) }
-    }
-
-    /// Access as a mutable slice.
-    fn as_mut_slice(&mut self) -> &mut [T] {
-        unsafe { std::slice::from_raw_parts_mut(self.as_mut_ptr(), self.len()) }
-    }
-
-    /// Unique identifier of the memory
-    ///
-    /// ```
-    /// # use ::accel::*;
-    /// # let device = Device::nth(0).unwrap();
-    /// # let ctx = device.create_context();
-    /// let mem1 = DeviceMemory::<i32>::new(&ctx, 12);
-    /// let mem2 = DeviceMemory::<i32>::new(&ctx, 12);
-    /// assert_ne!(mem1.id(), mem2.id());
-    /// ```
-    fn id(&self) -> u64 {
-        get_attr(
-            self.as_ptr(),
-            CUpointer_attribute::CU_POINTER_ATTRIBUTE_BUFFER_ID,
-        )
-    }
-
-    /// Memory Type
-    ///
-    /// ```
-    /// # use ::accel::*;
-    /// # let device = Device::nth(0).unwrap();
-    /// # let ctx = device.create_context();
-    /// let dev = DeviceMemory::<i32>::new(&ctx, 12);
-    /// assert_eq!(dev.memory_type(), MemoryType::CU_MEMORYTYPE_DEVICE);
-    /// let host = PageLockedMemory::<i32>::new(&ctx, 12);
-    /// assert_eq!(host.memory_type(), MemoryType::CU_MEMORYTYPE_HOST);
-    /// ```
-    fn memory_type(&self) -> MemoryType {
-        get_attr(
-            self.as_ptr(),
-            CUpointer_attribute::CU_POINTER_ATTRIBUTE_MEMORY_TYPE,
-        )
-    }
-}
-
-// Typed wrapper of cuPointerGetAttribute
-fn get_attr<T, Attr>(ptr: *const T, attr: CUpointer_attribute) -> Attr {
-    let data = MaybeUninit::uninit();
-    ffi_call!(
-        cuPointerGetAttribute,
-        data.as_ptr() as *mut _,
-        attr,
-        ptr as CUdeviceptr
-    )
-    .expect("Cannot get pointer attributes");
-    unsafe { data.assume_init() }
-}
 
 /// Memory allocated on the device.
 pub struct DeviceMemory<'ctx, T> {
--- accel/src/memory/mod.rs
+++ accel/src/memory/mod.rs
@@ -25,6 +25,10 @@ pub use device::*;
 
 use crate::{device::*, ffi_call};
 use cuda::*;
+use std::{
+    mem::MaybeUninit,
+    ops::{Deref, DerefMut},
+};
 
 /// Total and Free memory size of the device (in bytes)
 #[derive(Debug, Clone, Copy, PartialEq)]
@@ -81,4 +85,83 @@ mod tests {
         assert!(mem_info.total > mem_info.free);
         Ok(())
     }
+}
+
+/// Trait for CUDA managed memories. It assures
+///
+/// - can be accessed from both host (CPU) and device (GPU) programs
+/// - can be treated as a slice, i.e. a single span of either host or device memory
+///
+pub trait CudaMemory<T>: Deref<Target = [T]> + DerefMut {
+    /// Length of device memory
+    fn len(&self) -> usize;
+
+    /// Size of device memory in bytes
+    fn byte_size(&self) -> usize {
+        self.len() * std::mem::size_of::<T>()
+    }
+
+    /// Get the unified address of the memory as an immutable pointer
+    fn as_ptr(&self) -> *const T;
+
+    /// Get the unified address of the memory as a mutable pointer
+    fn as_mut_ptr(&mut self) -> *mut T;
+
+    /// Access as a slice.
+    fn as_slice(&self) -> &[T] {
+        unsafe { std::slice::from_raw_parts(self.as_ptr(), self.len()) }
+    }
+
+    /// Access as a mutable slice.
+    fn as_mut_slice(&mut self) -> &mut [T] {
+        unsafe { std::slice::from_raw_parts_mut(self.as_mut_ptr(), self.len()) }
+    }
+
+    /// Unique identifier of the memory
+    ///
+    /// ```
+    /// # use ::accel::*;
+    /// # let device = Device::nth(0).unwrap();
+    /// # let ctx = device.create_context();
+    /// let mem1 = DeviceMemory::<i32>::new(&ctx, 12);
+    /// let mem2 = DeviceMemory::<i32>::new(&ctx, 12);
+    /// assert_ne!(mem1.id(), mem2.id());
+    /// ```
+    fn id(&self) -> u64 {
+        get_attr(
+            self.as_ptr(),
+            CUpointer_attribute::CU_POINTER_ATTRIBUTE_BUFFER_ID,
+        )
+    }
+
+    /// Memory Type
+    ///
+    /// ```
+    /// # use ::accel::*;
+    /// # let device = Device::nth(0).unwrap();
+    /// # let ctx = device.create_context();
+    /// let dev = DeviceMemory::<i32>::new(&ctx, 12);
+    /// assert_eq!(dev.memory_type(), MemoryType::CU_MEMORYTYPE_DEVICE);
+    /// let host = PageLockedMemory::<i32>::new(&ctx, 12);
+    /// assert_eq!(host.memory_type(), MemoryType::CU_MEMORYTYPE_HOST);
+    /// ```
+    fn memory_type(&self) -> MemoryType {
+        get_attr(
+            self.as_ptr(),
+            CUpointer_attribute::CU_POINTER_ATTRIBUTE_MEMORY_TYPE,
+        )
+    }
+}
+
+// Typed wrapper of cuPointerGetAttribute
+fn get_attr<T, Attr>(ptr: *const T, attr: CUpointer_attribute) -> Attr {
+    let data = MaybeUninit::uninit();
+    ffi_call!(
+        cuPointerGetAttribute,
+        data.as_ptr() as *mut _,
+        attr,
+        ptr as CUdeviceptr
+    )
+    .expect("Cannot get pointer attributes");
+    unsafe { data.assume_init() }
 }

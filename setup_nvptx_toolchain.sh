072459e64e5f0a0af844accd185ea2d27a206266
Toshiki Teramura <toshiki.teramura@gmail.com>
2020-04-22 16:09:11 +0900

    Merge accel-derive test into test:accel

--- setup_nvptx_toolchain.sh
+++ setup_nvptx_toolchain.sh
@@ -4,5 +4,4 @@ set -xue
 NIGHTLY=nightly-2020-01-02
 rustup toolchain add ${NIGHTLY}
 rustup target add nvptx64-nvidia-cuda --toolchain ${NIGHTLY}
-rustup component add rustfmt --toolchain ${NIGHTLY}
 cargo install ptx-linker -f

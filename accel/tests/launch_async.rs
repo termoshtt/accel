c0bb3e42a4efd53621e2189266b1e8b8d867dd28
Toshiki Teramura <toshiki.teramura@gmail.com>
2020-05-28 02:56:58 +0900

    Add build test

---
+++ accel/tests/launch_async.rs
@@ -0,0 +1,6 @@
+#[test]
+fn launch_async_build_test() {
+    let t = trybuild::TestCases::new();
+    t.pass("tests/launch_async/temporal_value.rs");
+    t.compile_fail("tests/launch_async/mut_ref.rs");
+}

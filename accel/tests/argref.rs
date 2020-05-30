aeb5d3cd2ddbf54a74786b1f91009f981bbd0aaf
Toshiki Teramura <toshiki.teramura@gmail.com>
2020-05-30 17:51:39 +0900

    Add test for using reference in kernel

--- accel/tests/argref.rs
+++ accel/tests/argref.rs
@@ -1,7 +1,25 @@
 use accel::*;
 
 #[kernel]
-fn f1(a: &i32) {}
+fn f(a: &i32, b: &mut i32) {
+    accel_core::println!("&a = {:p}", a);
+    accel_core::println!("&b = {:p}", b);
+    *b = *a;
+}
 
-#[kernel]
-fn f2(a: &i32, b: &mut i32) {}
+#[test]
+fn copy() -> error::Result<()> {
+    let device = Device::nth(0)?;
+    let ctx = device.create_context();
+
+    let a = 1_i32;
+    let mut b = 0_i32;
+
+    println!("&a = {:p}", &a);
+    println!("&b = {:p}", &b);
+
+    f(&ctx, 1, 1, (&a, &mut b))?;
+
+    assert_eq!(a, b);
+    Ok(())
+}

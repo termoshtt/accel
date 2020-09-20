a323c51ad985abcdb560f165dfc4d37a3c26c1d7
jon-chuang <9093549+jon-chuang@users.noreply.github.com>
2020-09-20 17:10:33 +0800

    fix broken index() fn

--- accel-core/src/lib.rs
+++ accel-core/src/lib.rs
@@ -170,5 +170,5 @@ impl Idx3 {
 pub fn index() -> isize {
     let block_id = block_idx().into_id(grid_dim());
     let thread_id = thread_idx().into_id(block_dim());
-    (block_id + thread_id) as isize
+    (block_id * block_dim.size() + thread_id) as isize
 }
